a66e3a8155005ca11864cc2061383c1942e1633f
Ismael Henrique Da Silva <ismaelhrsilva@gmail.com>
2026-03-07 18:33:05 -0300

    ex04 ok

---
+++ ex04/whatis.py
@@ -0,0 +1,18 @@
+import sys
+
+def main():
+    if (len(sys.argv) == 1):
+        return
+    if (len(sys.argv) > 2):
+        print("AssertionError: more than one argument is provided")
+        return
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("AssertionError: argument is not an integer")
+        return
+    print("I'm Even.") if n % 2 == 0 else print("I'm Odd.")
+
+
+if __name__ == "__main__":
+    main()

d6833cb21dc49ed7fec1435f00c0246f78c6f4e8
Ismael Henrique da Silva <ismaelhrsilva@gmail.com>
2026-04-25 20:51:07 -0300

    wip list01/ex02

--- list01/ex01/array2D.py
+++ list01/ex01/array2D.py
@@ -32,6 +32,18 @@ def slice_me(family: list, start: int, end: int) -> list:
     if not isinstance(family, list):
         raise AssertionError("family in not a list")
 
+    if not all(isinstance(row, list) for row in family):
+        raise AssertionError("family must be a list of lists")
+
+    if len(family) == 0:
+        return []
+
+    row_length = len(family[0])
+    if not all(len(row) == row_length for row in family):
+        print("Error: inconsistent row sizes")
+        return []
+
+
     try:
         family_array = np.array(family)
         new_family_array = family_array[start:end]
--- list01/ex01/tester.py
+++ list01/ex01/tester.py
@@ -5,3 +5,12 @@ family = [[1.80, 78.4],
 [1.88, 75.2]]
 print(slice_me(family, 0, 2))
 print(slice_me(family, 1, -2))
+
+# caso com erro (listas de tamanhos diferentes)
+family_bad = [
+    [1.80, 78.4],
+    [2.15],              # erro aqui
+    [2.10, 98.5]
+]
+
+print(slice_me(family_bad, 0, 2))

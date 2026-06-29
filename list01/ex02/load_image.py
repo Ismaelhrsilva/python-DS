da07fc3f4d5c221e7dcaf227a6a872ac4a14a324
Ismael Henrique Da Silva <ismaelhrsilva@gmail.com>
2026-06-28 21:05:29 -0300

    list01 ex02 ok

--- list01/ex02/load_image.py
+++ list01/ex02/load_image.py
@@ -1,8 +1,19 @@
-from matplotlib import image as mpimg
+import os
+
 import numpy as np
+from matplotlib import image as mpimg
 
-def ft_load(path: str) -> array:
 
-    img = mpimg.imread(path)
+def ft_load(path: str) -> np.ndarray:
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Image not found: {path}")
 
-    print(f"The shape of image is: {img.shape}")
+    image = np.empty(0)
+    try:
+        image = mpimg.imread(path)
+        print(f"The shape of image is: {image.shape}")
+        print(image)
+    except Exception:
+        raise
+
+    return image
--- list01/ex02/tester.py
+++ list01/ex02/tester.py
@@ -1,3 +1,4 @@
 from load_image import ft_load
 
-print(ft_load("landscape.jpg"))
+print(ft_load("lanscape.jpg"))
+#print(ft_load("landscape.jpg"))

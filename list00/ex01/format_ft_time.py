27cf6a7a4f613b68cd063ab7192f1ff1cfe9375c
Ismael Henrique Da Silva <ismaelhrsilva@gmail.com>
2026-09-20 23:19:54 -0300

    list00 ex01

--- list00/ex01/format_ft_time.py
+++ list00/ex01/format_ft_time.py
@@ -1,12 +1,6 @@
 import time
-from datetime import datetime
-
-def print_time() -> tuple[str, str]:
-    epoch = time.time()
-    text = f"Seconds since January 1, 1970: {epoch:,.4f} or {epoch:.2e} in scientific notation"
-    time_str = datetime.now().strftime('%b %d %Y')
-    return text, time_str
-
-if __name__ == "__main__":
-    delta, now = print_time()
-    print(f"{delta}\n{now}")
+import datetime as dt
+epoch = time.time()
+text = f"Seconds since January 1, 1970: {epoch:,.4f} or {epoch:.2e} in scientific notation"
+print(text)
+print(dt.date.today().strftime("%b %d %Y"))
